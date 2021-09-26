59bc245bf499474592a5757c80f40a757af2e02b
ravehBS <ravehsbs@gmail.com>
2021-09-26 18:22:13 +0000

    calculate misclassification rate for a trained embedding

--- evaluate_model.py
+++ evaluate_model.py
@@ -15,11 +15,11 @@ evaluation metrics such as: Dasgupta cost, familiarity(?).
 
 
 os.makedirs("tmp", exist_ok=True)
-"""
+
 model_path = "/home/raveh.be@staff.technion.ac.il/PycharmProjects/GED/embeddings/breast_cancer/best_cityblock_full/model_sd1234_epch15.pkl"
 save_path = "tmp/best_cityblock.png"
 feature_dim = 98
-"""
+
 
 """
 model_path = "/home/raveh.be@staff.technion.ac.il/PycharmProjects/GED/embeddings/breast_cancer/best_mahalanobis_full/model_sd1234_epch15.pkl"
@@ -27,10 +27,11 @@ save_path = "tmp/best_mahalanobis.png"
 feature_dim = 181
 """
 
+"""
 model_path = "/home/raveh.be@staff.technion.ac.il/PycharmProjects/GED/embeddings/breast_cancer/best_cosine_full/model_sd1234_epch57.pkl"
 save_path = "tmp/best_cosine.png"
 feature_dim = 103
-
+"""
 
 """
 model_path = "/home/raveh.be@staff.technion.ac.il/PycharmProjects/GED/embeddings/breast_cancer/best_euclidean_full/model_sd1234_epch9.pkl"
@@ -78,52 +79,71 @@ def calculate_dasgupta_cost(model_path, save_path, feature_dim):
     print(f"Dasgupta cost: {cost}")
     return cost
 
-####
-tree = nx.DiGraph()
-#leaves
-tree.add_node('b',label=0)
-tree.add_node('c',label=1)
-tree.add_node('d',label=1)
-#parents
-tree.add_node('root',label=None)
-tree.add_node('a',label=None)
-#edges
-tree.add_edge('root','a')
-tree.add_edge('root','b')
-tree.add_edge('a','c')
-tree.add_edge('a','d')
+#########
+def calculate_misclassification(model_path, save_path, feature_dim):
+    method = 'euclidean' #doesn't really matter
+    ##########
+    model = None #load model
+    with open(model_path,'rb') as f:
+        #model = torch.load(f)
+        model = HypHC(n_nodes=num_data_samples)
+        model.load_state_dict(torch.load(f))
+    tree = model.decode_tree(fast_decoding=True) # tree is digraph
 
+    #first n in tree are the leaves, ordered in the same way as the labels
+    #because we load the data the same way as in the training
+    x, y_true, similarities, label_dict = load_hypbc_multi_group(num_groups=1,
+                                                                     num_data_samples=num_data_samples,
+                                                                     feature_dim=feature_dim,
+                                                                     method=method,
+                                                                     feature_correlation_thresh=feature_correlation_thresh,
+                                                                     visualize=False)
 
-# Pseudo code
-"""
-get_histogram(node):
-    sons = get_succesors(node)
-    if sons == None:
-        #is leaf
-        node.histogram[node,label] = 1
-    else:
-        #is parent
-        node.histogram = get_histogram(sons[0]) + get_histogram(sons[1])
+    # initial stats
+    n = len(y_true[0])
+    histograms = np.zeros((2*n-1,len(label_dict)))
 
-    return node.histogram
+    #fill histograms for leaves
+    for leaf, label in enumerate(y_true[0]):
+        histograms[leaf][label] = 1
 
-get_label(node):
-    if node.histogram != None:
-        return max(node.histogram)
-    else: #we dont have histogram, need to find it
-        node.histogram = get_histogram(node)
-        return max(node.histogram)
+    def get_histogram(node):
+        sons = tree.successors(node)
 
+        for son in sons: #if entered this loop, node is parent
+            histograms[node] += get_histogram(son)
 
+        return histograms[node]
 
-for each leaf:
-    brother = find_brother()
-    if brother.label == None:
-        brother.label = get_label(brother)
+    def get_label(node):
+        if any(histograms[node] > 0): #then we already have a histogram
+            return np.argmax(histograms[node])
+        else: #we dont have histogram, need to find it
+            histograms[node] = get_histogram(node)
+            return np.argmax(histograms[node])
 
-    if leaf.label != brother.label:
-        cnt += 1
-    else:
-        continue
+    def get_brother(node):
+        father = tree.predecessors(node).__next__() #get the father node number
+        children = tree.successors(father)
 
-"""
+        for child in children:
+            if child is not node:
+                return child
+
+        assert False
+
+    #calculate the missclassification rate
+    mismatch_counter = 0.0
+    for node in tree:
+        if node >= n: #then this node is not a leaf. no need to calculate its familiarity
+            continue
+        brother = get_brother(node)
+
+        if get_label(node) != get_label(brother):
+            mismatch_counter += 1.0
+        else:
+            continue
+
+    misclassification_rate = mismatch_counter/n
+    print(f'misclassification_rate is : {misclassification_rate}')
+
